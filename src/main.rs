ae88216923f52806650162089c716dfcd13ff5ab
Nikita Hismatov <hismatovns@gmail.com>
2021-05-21 00:04:09 +0500

    wip...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.iter().count() == 1 {
+        println!("No url specified");
+        exit(1);
+    }
+
+    let start_url = &args[1];
+    println!("{:?}", start_url);
 }
