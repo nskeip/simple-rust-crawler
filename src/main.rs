ec4d8263e2529733d4a0511f23da22ebdd903bbc
Nikita Hismatov <hismatovns@gmail.com>
2021-05-24 01:07:37 +0500

    async-std

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate async_std;
 extern crate reqwest;
 extern crate select;
 
@@ -5,17 +6,91 @@ use std::env;
 use std::collections::HashSet;
 use std::process::exit;
 
-use reqwest::Url;
+use reqwest::{Url, Error};
 
-use futures;
+use futures::future::{join_all};
 use select::document::Document;
 use select::predicate::{Name};
-use tokio::task;
 
-#[tokio::main]
-async fn main() {
-    const MAX_HEIGHT: i8 = 1;
+use tokio::runtime::Runtime;
 
+use async_std::{
+    prelude::*,
+    task,
+};
+
+const MAX_HEIGHT: i8 = 2;
+
+async fn process_url(url: Url) -> Result<(String, Vec<String>), Error> {
+    let resp = reqwest::get(url.clone()).await?;
+    let text = resp.text().await?;
+    let doc = Document::from(text.as_str());
+    let mut resulting_vector = Vec::new();
+    for node in doc.select(Name("a")) {
+        if let Some(href) = node.attr("href") {
+            resulting_vector.push(href.to_string());
+        }
+    }
+    Ok((text, resulting_vector))
+}
+
+async fn run(start_url: Url) -> Result<(), Error> {
+    let start_domain = start_url
+        .domain()
+        .expect("Strange domain, you know?");
+
+    let mut current_floor_queue: Vec<Url>;
+    let mut next_floor_queue = vec![start_url.clone()];
+    let mut visited_pages: HashSet<Url> = HashSet::new();
+
+    let mut current_floor = 0;
+    let mut siblings_on_current_floor = 1;
+
+    while current_floor <= MAX_HEIGHT {
+        current_floor_queue = next_floor_queue;
+        next_floor_queue = Vec::new();
+
+        let mut proc_url_futures =
+            Vec::with_capacity(siblings_on_current_floor);
+
+        // собираем все подряд
+        for url in current_floor_queue.clone() {
+            if visited_pages.contains(&url) {
+                continue;
+            }
+            visited_pages.insert(url.clone());
+            proc_url_futures.push(process_url(url));
+        }
+
+        let proc_results = futures::future::join_all(proc_url_futures).await;
+        for (i, r) in proc_results.iter().enumerate() {
+            if let Ok((page_text, page_links)) = r {
+                for href in page_links {
+                    if href.starts_with('#') {
+                        continue;
+                    }
+
+                    let url = current_floor_queue[i].clone();
+
+                    if let Ok(new_absolute_url) = url.join(href) {
+                        if let Some(new_domain) = new_absolute_url.domain() {
+                            if new_domain.to_string() != start_domain {
+                                continue;
+                            }
+                        }
+                        next_floor_queue.push(new_absolute_url);
+                    }
+                }
+            }
+        }
+
+        current_floor += 1;
+    }
+
+    Ok(())
+}
+
+fn main() {
     let args: Vec<String> = env::args().collect();
     if args.iter().count() == 1 {
         println!("No url specified");
@@ -23,67 +98,6 @@ async fn main() {
     }
 
     let start_url = Url::parse(&args[1]).expect("Incorrect start url");
-    let start_domain = start_url.domain().expect("Strange domain, you know?").to_string();
-    let mut queue = vec![start_url];
 
-    let mut visited_pages: HashSet<Url> = HashSet::new();
-
-    let mut current_floor = 0;
-    let mut siblings_on_current_floor = 1;
-    let mut siblings_on_next_floor = 0;
-
-    while current_floor <= MAX_HEIGHT && !queue.is_empty() {
-        let mut futures = Vec::with_capacity(siblings_on_current_floor);
-        for _ in 0..siblings_on_current_floor {
-            let url = queue.remove(0);
-            visited_pages.insert(url.clone());
-
-            let future = tokio::spawn(async move {
-                let resp = reqwest::get(url.clone()).await;
-                if resp.is_err() {
-                    println!("Failed to download {}", url);
-                    return;
-                }
-
-                let text = resp.unwrap().text().await;
-                if text.is_err() {
-                    println!("Failed to parse {}", url);
-                    return;
-                }
-
-                let doc = Document::from(text.unwrap().as_str());
-
-                for node in doc.select(Name("a")) {
-                    if let Some(href) = node.attr("href") {
-                        if href.starts_with('#') {
-                            return;
-                        }
-
-                        if let Ok(new_absolute_url) = url.join(href) {
-                            if let Some(new_domain) = new_absolute_url.domain() {
-                                if new_domain.to_string() != start_domain {
-                                    continue;
-                                }
-                            }
-
-                            if visited_pages.contains(&new_absolute_url.clone()) {
-                                return;
-                            }
-                            queue.push(new_absolute_url);
-                            siblings_on_next_floor += 1;
-                        }
-                    }
-                };
-            });
-            futures.push(future);
-        }
-        futures::join!();
-        for f in futures {
-            f.await;
-        }
-
-        current_floor += 1;
-        siblings_on_current_floor = siblings_on_next_floor;
-        siblings_on_next_floor = 0;
-    }
+    Runtime::new().expect("Failed to create runtime").block_on(run(start_url));
 }
